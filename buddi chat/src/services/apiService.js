2e0d5ff95ac5deaf44e47aafaa6af8a3d90eda9f
Chipatenii <innocentmanda70@gmail.com>
2024-12-30 20:53:47 +0200

    update

--- buddi chat/src/services/apiService.js
+++ buddi chat/src/services/apiService.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+// Removed dotenv import and config as it's handled by dotenv-webpack
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000', // Use environment variable for base URL or fallback to localhost
