657c2d7bd2497c66f522a672d173b66cbaa10554
Chipatenii <innocentmanda70@gmail.com>
2024-12-30 20:58:27 +0200

    update

--- buddi chat/src/services/apiService.js
+++ buddi chat/src/services/apiService.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 // Removed dotenv import and config as it's handled by dotenv-webpack
 
 const api = axios.create({
+    // eslint-disable-next-line no-undef
     baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000', // Use environment variable for base URL or fallback to localhost
 });
 
