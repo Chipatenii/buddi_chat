14b69f157775c78a27d12baab2b9c322b989629b
Chipatenii <innocentmanda70@gmail.com>
2025-01-27 14:42:18 +0200

    defined fetch

--- buddi chat/server/server.js
+++ buddi chat/server/server.js
@@ -133,7 +133,7 @@ app.use((req, res) => {
 
 // ========================== Start Server ==========================
 const server = http.createServer(app);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
